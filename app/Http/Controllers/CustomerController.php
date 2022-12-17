4bb973288bd04013353c96a543d1c29ac3cabae8
katsuki.chinen <sss.khy1221@gmail.com>
2022-12-17 14:46:13 +0900

    顧客検索機能の追加

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCustomerRequest;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use Inertia\Inertia;
@@ -14,9 +15,12 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $customers = Customer::select('id' , 'name', 'kana', 'tel')->paginate(50);
+
+        $customers = Customer::searchCustomers($request->search)
+        ->select('id', 'name', 'kana', 'tel')->paginate(50);
+    //dd($customers);
         return Inertia::render('Customers/Index', [
             'customers' => $customers
         ]);
