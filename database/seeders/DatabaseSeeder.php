70fa6cca72b469ca7887effe30e5c1b3c8c00467
katsuki.chinen <sss.khy1221@gmail.com>
2022-12-18 13:56:09 +0900

    購買データ作成 customerテーブルと紐付け

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Customer;
+use App\Models\Purchase;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +22,7 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class
         ]);
         Customer::factory(100)->create();
+        Purchase::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
