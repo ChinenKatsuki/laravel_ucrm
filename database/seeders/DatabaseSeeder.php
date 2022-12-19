d8da4b5086cc6d54a0b1f15671b79f122e14e818
katsuki.chinen <sss.khy1221@gmail.com>
2022-12-19 23:18:16 +0900

    [作成] item_purchaseにダミーデータを追加する機能

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,18 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class
         ]);
         Customer::factory(100)->create();
-        Purchase::factory(100)->create();
+        $items = \App\Models\Item::all();
+        //Purchaseを登録時 中間テーブルにも同時に登録する (1件の購入情報に1件~3件の商品情報を登録とする)
+        //each・・1件ずつ処理
+        //attach・・中間テーブルに情報登録 外部キー以外で中間テーブルに情報追加するには第2引数に書く
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items){
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),
+                // 1~3個のitemをpurchaseにランダムに紐づけ
+                ['quantity' => rand(1, 5) ]
+            );
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
