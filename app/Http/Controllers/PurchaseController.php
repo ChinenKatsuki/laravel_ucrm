246a5cc37810f9fcc39b2817ebd8816b7868a4f8
katsuki.chinen <sss.khy1221@gmail.com>
2022-12-22 17:22:08 +0900

    【作成】購入画面

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePurchaseRequest;
 use App\Http\Requests\UpdatePurchaseRequest;
+use Inertia\Inertia;
 use App\Models\Purchase;
+use App\Models\Item;
+use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
+
 
 class PurchaseController extends Controller
 {
@@ -25,7 +30,13 @@ class PurchaseController extends Controller
      */
     public function create()
     {
-        //
+        //$customers = Customer::select('id', 'name', 'kana')->get();
+        $items = Item::select('id', 'name', 'price')->where('is_selling', 1)->get();
+
+        return Inertia::render('Purchases/Create', [
+            //'customers' => $customers,
+            'items' => $items
+        ]);
     }
 
     /**
@@ -36,7 +47,28 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        //
+        DB::beginTransaction();
+        try{
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status
+            ]);
+    
+            foreach($request->items as $item){
+                //attach 中間テーブルに情報を設定することができる。
+                //第一引数で紐づくデータベーステーブルのIDを入れる必要がある。
+                //第二引数は連想配列で書く必要がある
+                $purchase->items()->attach($purchase->id, [
+                    'item_id' => $item['id'],
+                    'quantity' => $item['quantity']
+                ]);
+            }
+            DB::commit();
+            return to_route('dashboard');
+    
+        }catch(\Exception $e){
+            DB::rollback();
+        }
     }
 
     /**
